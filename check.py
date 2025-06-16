4350550f37a71bd738e997c67bb08ed776c51f74
akharitonov-ph-ns1 <lesaharitonov9552@gmail.com>
2025-06-16 19:04:24 +0700

    Update check.py
    Улучшено условие для функции: теперь функция проверяет, стоит ли занятие не только в предыдущий день, но и в следующий

--- check.py
+++ check.py
@@ -1,9 +1,10 @@
 def check(current_schedule, current_day, class_name):
     days_order = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
     current_day_index = days_order.index(current_day)
-    if current_day_index > 0:
+    if current_day_index > 0 and current_day_index < 6:
         previous_day = days_order[current_day_index - 1]
-        if class_name in current_schedule[previous_day]:
+        next_day = days_order[current_day_index + 1]
+        if class_name in current_schedule[previous_day] or class_name in current_schedule[next_day]:
             return True
         else:
             return False
