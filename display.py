efc897f8952eea248570509af24960257facd4e3
akharitonov-ph-ns1 <lesaharitonov9552@gmail.com>
2025-06-26 23:04:11 +0700

    Update display.py
    Введена нумерация занятий

--- display.py
+++ display.py
@@ -1,7 +1,11 @@
 def screen(current_schedule):
+    print('')
     print("Текущее расписание: ")
+    print('')
     for day, classes in current_schedule.items():
         if classes:
-            print(f"{day}: {', '.join(classes)}")
+            print(f"{day}:")
+            for i, class_name in enumerate(classes, 1):
+                print(f" {i}. {class_name}")
         else:
             print(f"{day}: Нет занятий")
