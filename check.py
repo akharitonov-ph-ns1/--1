cc8d3be761e8add89e9eecc8612aeb2623120a9c
akharitonov-ph-ns1 <lesaharitonov9552@gmail.com>
2025-06-26 23:03:12 +0700

    Update check.py

--- check.py
+++ check.py
@@ -1,10 +1,22 @@
 def check(current_schedule, current_day, class_name):
-    days_order = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+    days_order = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
     current_day_index = days_order.index(current_day)
-    if current_day_index > 0 and current_day_index < 6:
+    if current_day_index > 0 and current_day_index < 5:
         previous_day = days_order[current_day_index - 1]
         next_day = days_order[current_day_index + 1]
         if class_name in current_schedule[previous_day] or class_name in current_schedule[next_day]:
+            return True
+        else:
+            return False
+    elif current_day_index == 0:
+        next_day = days_order[1]
+        if class_name in current_schedule[next_day]:
+            return True
+        else:
+            return False
+    elif current_day_index == 5:
+        previous_day = days_order[4]
+        if class_name in current_schedule[previous_day]:
             return True
         else:
             return False
