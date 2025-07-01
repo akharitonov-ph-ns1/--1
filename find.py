8b433231cefdc084e38788182a58ffe4649a8c77
akharitonov-ph-ns1 <lesaharitonov9552@gmail.com>
2025-07-01 17:42:44 +0700

    Update find.py

--- find.py
+++ find.py
@@ -1,12 +1,21 @@
-def find(current_schedule):
-    class_name = input("Введите название занятия для поиска: ")
-    f = False
-    print(f"Поиск занятия '{class_name}':")
-    for day, classes in current_schedule.items():
-        for c in classes:
-            count = [c.lower() for c in classes].count(class_name.lower())
-            if count > 0:
-                print(f" - {day} (встречается {count} раз(а))")
-                f = True      
-    if f == 0:
-        print("Занятие не найдено в расписании.")
+from sort_bin_search import sort_schedule
+from sort_bin_search import binary_search
+
+def find(schedule):
+    class_name = input("Введите название занятия для поиска: ").strip()
+    if not class_name:
+        print("Ошибка: пожалуйста, введите название занятия.")
+        return
+    sort_schedule(schedule)
+    found_days = []
+    for day, classes in schedule.items():
+        indices = binary_search(classes, class_name)
+        if indices:
+            found_days.append((day, len(indices)))
+
+    if found_days:
+        print(f"\nЗанятие '{class_name}' найдено:")
+        for day, count in found_days:
+            print(f"- {day} (встречается {count} раз(а))")
+    else:
+        print("\nЗанятие не найдено в расписании.")
