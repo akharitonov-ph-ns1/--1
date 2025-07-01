1560748d0841123c57382ed7b774d36924abfefe
akharitonov-ph-ns1 <lesaharitonov9552@gmail.com>
2025-07-01 17:45:16 +0700

    Create sort_bin_search.py
    Реализация двоичного поиска после обычной сортировки методом sorted() по алфавиту

---
+++ sort_bin_search.py
@@ -0,0 +1,28 @@
+def sort_schedule(schedule):
+    for day in schedule:
+        schedule[day].sort()
+
+def binary_search(arr, goal):
+    left = 0
+    right = len(arr) - 1
+    result = []
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == goal:
+            result.append(mid)
+            i = mid - 1
+            while i >= 0 and arr[i] == goal:
+                result.append(i)
+                i -= 1
+            i = mid + 1
+            while i < len(arr) and arr[i] == goal:
+                result.append(i)
+                i += 1
+            break
+        elif arr[mid] < goal:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return sorted(result)
