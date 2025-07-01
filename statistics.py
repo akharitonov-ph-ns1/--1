d5d2e95fbc18479ff0b76ea41f1161cdb161463a
akharitonov-ph-ns1 <lesaharitonov9552@gmail.com>
2025-07-01 17:46:36 +0700

    Create statistics.py
    Небольшая функция вывода статистики кол-ва уроков по дням для оценки загруженности

---
+++ statistics.py
@@ -0,0 +1,9 @@
+def statistics(current_schedule):
+    print("Загруженность по дням:")
+    for day, classes in current_schedule.items():
+        if len(classes) > 1 and len(classes) < 5:
+            print(f"{day}: {len(classes)} занятия")
+        elif len(classes) == 1:
+            print(f"{day}: 1 занятие")
+        else:
+            print(f"{day}: {len(classes)} занятий")
